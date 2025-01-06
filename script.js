94f77479e7e8a10e54934c02303985b4b2f5e846
vivek-jain-17 <vivek.v.jain17@gmail.com>
2025-01-06 15:41:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const splashScreen = document.getElementById('splash-screen');
+    const mainContent = document.getElementById('main-content');
+
+    setTimeout(() => {
+        splashScreen.style.display = 'none'; 
+        mainContent.style.display = 'block'; 
+    }, 2000);
+});
+
+
+function showProject(projectId) {
+    const projectDetails = document.getElementById("project-details");
+    const projectVideo = document.getElementById("project-video");
+    const githubLink = document.getElementById("github-link");
+
+    
+    if (projectId === "project1") {
+        projectVideo.src = "img&vid/to-do-web-video.mp4"; 
+        githubLink.href = "https://github.com/vivek-jain-17/to-do-list.git";
+    } else if (projectId === "project2") {
+        projectVideo.src = "img&vid/blog-web-video.mp4"; 
+        githubLink.href = "https://github.com/vivek-jain-17/Bloggigng-Site.git";
+    } else if (projectId === "project3") {
+        projectVideo.src = "img&vid/tictac-web-video.mp4"; 
+        githubLink.href = "https://github.com/vivek-jain-17/tic-tac-toe.git";
+    } else if (projectId === "project4") {
+        projectVideo.src = "xyz.mp4"; 
+        githubLink.href = "https://github.com/your-repo/project2";
+    }
+
+    projectDetails.classList.remove("hidden");
+}
+
+
+function closeProject() {
+    const projectDetails = document.getElementById("project-details");
+    const projectVideo = document.getElementById("project-video");
+
+    projectVideo.pause(); 
+    projectVideo.src = ""; 
+    projectDetails.classList.add("hidden");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const bubbleContainer = document.querySelector(".bubbles");
+
+    for (let i = 0; i < 50; i++) {
+        const bubble = document.createElement("span");
+        const size = Math.random() * 50 + 10; 
+        bubble.style.width = `${size}px`;
+        bubble.style.height = `${size}px`;
+        bubble.style.left = `${Math.random() * 100}%`; 
+        bubble.style.animationDuration = `${Math.random() * 10 + 5}s`; 
+        bubble.style.animationDelay = `${Math.random() * 5}s`; 
+        bubbleContainer.appendChild(bubble);
+    }
+});
